67b25d96f82be0fd13e991adaa0e964699c08a58
lihaixin <lihaixin@15099.net>
2021-05-21 10:11:38 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,15 +8,7 @@ if [ ! -e /dev/net/tun ]; then
 fi
 
 get_ip() {
-	[[ -z $ip ]] && ip=$(curl -s https://ipinfo.io/ip)
-	[[ -z $ip ]] && ip=$(curl -s https://api.ip.sb/ip)
-	[[ -z $ip ]] && ip=$(curl -s https://api.ipify.org)
-	[[ -z $ip ]] && ip=$(curl -s https://ip.seeip.org)
-	[[ -z $ip ]] && ip=$(curl -s https://ifconfig.co/ip)
-	[[ -z $ip ]] && ip=$(curl -s https://api.myip.com | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
-	[[ -z $ip ]] && ip=$(curl -s icanhazip.com)
-	[[ -z $ip ]] && ip=$(curl -s myip.ipip.net | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}")
-	[[ -z $ip ]] && echo -e "\n$red 这垃圾小鸡扔了吧！$none\n" && exit 1
+	ip=$(curl -s https://ipinfo.io/ip) || ip=$(curl -s https://api.ip.sb/ip) || echo -e "\n$red 这垃圾小鸡扔了吧！$none\n"
 }
 zerotier-one &
 sleep 5
@@ -47,5 +39,6 @@ if [ ! -d "/var/lib/zerotier-one/moons.d" ]; then
   create_moon
 fi
 
+kill -HUP `pidof zerotier-one`
 while true; do sleep 1; done;
 
